baef244bb4764efe8f2103500cdbc7e7797357d6
Beth <beth.williams@bluewin.ch>
2025-01-22 23:45:31 +0100

    messing things up

--- analysis/syntaxtree.go
+++ analysis/syntaxtree.go
@@ -2,6 +2,7 @@ package analysis
 
 import (
 	"borm-lsp/lsp"
+	"fmt"
 	"log"
 	"strings"
 )
@@ -11,20 +12,17 @@ type SynType string
 const (
 	TEXT SynType = "text"
 	FUNCTION = "function"
+	DECLARATION = "declaration"
+	NAME = "name"
 	FIELD = "field" 
 	VARIABLE = "variable" 
-	CLASS = "class" 
-	INTERFACE = "interface" 
 	VALUE = "value" 
-	ENUM = "enum" 
 	KEYWORD = "keyword" 
 	FILE = "file" 
-	ENUMMEMBER = "enummember" 
-	CONSTANT = "constant" 
-	TYPEPARAMETER = "typeparameter" 
 	BLOCK = "block" 
 	COMMENT = "comment" 
 	INCLUDE = "include" 
+	TYPE = "type"
 )
 
 type SyntaxNode struct {
@@ -35,17 +33,19 @@ type SyntaxNode struct {
 	Value string
 	Type SynType
 	IsBad bool
+	Diagnostics []string
 }
 
-func NewNode(par *SyntaxNode, val string, t SynType, start, end lsp.Position) SyntaxNode {
-	return SyntaxNode{
-		Children: []SyntaxNode{},
-		Parent: par,
-		Start: start,
+func NewNode(parent *SyntaxNode, start, end lsp.Position, value string, t SynType) SyntaxNode {
+	return SyntaxNode {
+		Children: []SyntaxNode{}, 
+		Parent: parent,
+		Start: start, 
 		End: end,
-		Value: val,
-		Type: t,
+		Value: value, 
+		Type: t, 
 		IsBad: false,
+		Diagnostics: []string{},
 	}
 }
 
@@ -89,9 +89,23 @@ func CreateTree(logger *log.Logger, doc, text string) SyntaxNode {
 		return SyntaxNode{}
 	}
 
-	root := NewNode(nil, doc, FILE, GetStartPos(tokens...), GetFinalPos(tokens...))
+	root := SyntaxNode{
+		Value:doc, 
+		Type: FILE, 
+		Start: GetStartPos(tokens...), 
+		End: GetFinalPos(tokens...),
+	}
 
 	return root.createBranch(logger, tokens)
+}
+
+func (n SyntaxNode) passedHeader() bool {
+	for _, child := range n.Children {
+		if child.Type != COMMENT && child.Type != INCLUDE {
+			return true
+		}
+	}
+	return false
 }
 
 func (n SyntaxNode) createBranch(logger *log.Logger, tokens []Token) SyntaxNode {
@@ -110,72 +124,184 @@ func (n SyntaxNode) createBranch(logger *log.Logger, tokens []Token) SyntaxNode 
 			i += jump+1
 			continue
 		}
-		if token.value == "#include" {
+		switch token.value {
+		case "#include":
 			//it's an include statement 
 			node, jump := createIncludeNode(tokens[i:])
 			node.Parent = &n
+			if n.Type != FILE || n.passedHeader() {
+				// is bad
+				node.IsBad = true
+				node.Diagnostics = append(node.Diagnostics, "include statements must appear before other declarations.")
+			} 
 			n.Children = append(n.Children, node)
-			i += jump+1
-			continue
-		}
-		if token.value == "function" {
-			/*
-			// it's function
-			node := createBlockNode(tokens[i:])
+			i += jump
+		case "void":
+			// it's a function
+			node, jump := createFunctionNode(tokens[i:])
 			node.Parent = &n
 			n.Children = append(n.Children, node)
-			i = node.End.Line
-			line = lines[i]
-			charPos = node.End.Character
-			*/
+			i += jump
+		case "int", "double", "float", "char", "bool", "string":
+			//it's a declaration. could be a function
+			node, jump := createDeclarationNode(tokens[i:])
+			node.Parent = &n
+			n.Children = append(n.Children, node)
+			i += jump
+
+		default:
+			// unhandled/text token
+			node := NewNode(&n, token.pos, GetFinalPos(token), token.value, TEXT)
+			n.Children = append(n.Children, node)
+			i++
 		}
-		// unhandled/text token
-		node := NewNode(&n, token.value, TEXT, token.pos, GetFinalPos(token))
-		n.Children = append(n.Children, node)
-		i++
 	}
 	return n
 }
 
+func createDeclarationNode(tokens []Token) (SyntaxNode, int) {
+	if len(tokens) == 1 {
+		//is bad nothing after type
+		node := NewNode(nil, GetStartPos(tokens[0]), GetFinalPos(tokens[0]), tokens[0].value, TYPE)
+		node.IsBad = true
+		node.Diagnostics = append(node.Diagnostics, fmt.Sprintf("%s (type) is not an expression", tokens[0].value))
+		return node, 1
+	}
+
+	if tokens[1].value == "function" {
+		// it's a function
+		return createFunctionNode(tokens)
+	}
+
+	//it's a variable
+	return createVariableNode(tokens)
+}
+
+func createFunctionNode(tokens []Token) (SyntaxNode, int) {
+	firstLine := GetTokensToNewLine(tokens)
+	headNode, name, err := createFunctionHeadNode(firstLine)
+	if err != nil {
+		// is bad, function not properly formed
+		headNode.IsBad = true
+		headNode.Diagnostics = append(headNode.Diagnostics, err.Error())
+	}
+
+	node := NewNode(nil, GetStartPos(firstLine...), headNode.End, name, FUNCTION)
+	node.Children = append(node.Children, headNode)
+	if headNode.Type == DECLARATION {
+		return node, len(firstLine)
+	}
+
+	bodyNode, err := createFunctionBodyNode(tokens[len(firstLine):])
+	if err != nil {
+		// is bad, function not properly formed
+		bodyNode.IsBad = true
+		bodyNode.Diagnostics = append(bodyNode.Diagnostics, "Malformed function")
+	}
+	node.Children = append(node.Children, bodyNode)
+
+	return node, len(firstLine)
+}
+
+func createFunctionHeadNode(tokens []Token) (SyntaxNode, string, error) {
+	node := NewNode(nil, GetStartPos(tokens...), GetFinalPos(tokens...), Stringify(tokens), DECLARATION)
+	if len(tokens) == 1 {
+		return node, "", fmt.Errorf("%s is not an expression", tokens[0].value)		
+	}
+	typeNode := NewNode(&node, GetStartPos(tokens[0]), GetFinalPos(tokens[0]), tokens[0].value, TYPE)
+	node.Children = append(node.Children, typeNode)
+
+	if tokens[1].value != "function" {
+		return node, "", fmt.Errorf("Malformed function declaration")		
+	}
+	keyWordNode := NewNode(&node, GetStartPos(tokens[1]), GetFinalPos(tokens[1]), tokens[1].value, KEYWORD)
+	node.Children = append(node.Children, keyWordNode)
+
+	if len(tokens) == 2 {
+		return node, "", fmt.Errorf("A function must have a name")
+	}
+
+	if !strings.Contains(tokens[2].value, "(") {
+		//deal with this later i guess
+	}
+
+	name, params, _ := strings.Cut(tokens[2].value, "(")
+	start := GetStartPos(tokens[2])
+	end := lsp.Position{Line:start.Line, Character:start.Character + len(name)}
+	nameNode := NewNode(&node, start, end, name, NAME)
+
+	params, rest, found := strings.Cut(params, ")")
+	if found {
+		// we have no params
+		if strings.TrimSpace(rest)[0] == ';' {
+			// it's a declaration
+			return node, name, nil
+		} 
+		node.Type = FUNCTION 
+		return node, name, nil
+	}
+
+	paramNodes := []SyntaxNode{}
+	if len(params) > 0 {
+		paramstart := lsp.Position{Line:start.Line, Character: end.Character+2}
+		paramend := lsp.Position{Line:start.Line, Character: paramstart.Character+len(params)}
+		paramNodes = append(paramNodes, NewNode(&node, paramstart, paramend, params, TYPE))
+	}
+
+	node.Children = append(node.Children, nameNode)
+	node.Children = append(node.Children, paramNodes...)
+
+	if len(tokens) == 3 {
+		return node, "", fmt.Errorf("Malformed function.")
+	}
+
+	return node, nameNode.Value, nil
+}
+
+func createVariableNode(tokens []Token) (SyntaxNode, int) {
+	node := NewNode(nil, GetStartPos(tokens...), GetFinalPos(tokens...), Stringify(tokens), VARIABLE)
+	return node, 0
+}
+
 func createCommentNode(tokens []Token) (SyntaxNode, int) {
 	tokens = GetTokensToNewLine(tokens)
-	spent := len(tokens)-1
-	value := Stringify(tokens)
-	finalPos := GetFinalPos(tokens...)
-	node := NewNode(nil, value, COMMENT, tokens[0].pos, finalPos)
-	return node, spent
+	node := NewNode(nil, GetStartPos(tokens...), GetFinalPos(tokens...), Stringify(tokens), COMMENT)
+	return node, len(tokens)
 }
 
 func createIncludeNode(tokens []Token) (SyntaxNode, int) {
 	tokens = GetTokensToNewLine(tokens)
-	spent := len(tokens)-1
-	finalPos := GetFinalPos(tokens...)
-	value := Stringify(tokens)
-	node := NewNode(nil, value, INCLUDE, tokens[0].pos, finalPos)
+	spent := len(tokens)
+	node := NewNode(nil, tokens[0].pos, GetFinalPos(tokens...), Stringify(tokens), INCLUDE)
 
-	keywordNode := NewNode(&node, "include", KEYWORD, tokens[0].pos, GetFinalPos(tokens[0]))
+	keywordNode := NewNode(&node, tokens[0].pos, GetFinalPos(tokens[0]), "include", KEYWORD)
 	node.Children = append(node.Children, keywordNode)
 	
 	if len(tokens) < 2 {
 		node.IsBad = true
+		node.Diagnostics = append(node.Diagnostics, "You must supply a file or header to be included.")
 		return node, spent
 	}
 	if len(tokens) > 2 {
 		node.IsBad = true
+		node.Diagnostics = append(node.Diagnostics, "Syntax error. Please attach a single header or file per include.")
 	}
 
-	valueNode := NewNode(&node, "", VALUE, tokens[1].pos, GetFinalPos(tokens[1]))
+	valueNode := NewNode(&node, tokens[1].pos, GetFinalPos(tokens[1]), "", VALUE)
 	switch tokens[1].value[0] {
 	case '"':
 		if tokens[1].value[len(tokens[1].value)-1] != '"' {
 			valueNode.IsBad = true
+			valueNode.Diagnostics = append(valueNode.Diagnostics, "Unclosed quotation marks.")
 		}
 	case '<':
 		if tokens[1].value[len(tokens[1].value)-1] != '>' {
 			valueNode.IsBad = true
+			valueNode.Diagnostics = append(valueNode.Diagnostics, "Unclosed angle brackets.")
 		}
 	default:
 		valueNode.IsBad = true
+		valueNode.Diagnostics = append(valueNode.Diagnostics, "Syntax error. Accepted include formats: <header.h> or \"file_path\".")
 	}
 	valueNode.Value = strings.Trim(tokens[1].value, "\"<>")
 
@@ -205,3 +331,4 @@ func createFileNode(text string, line, charPos int) SyntaxNode {
 	}
 	return node
 }
+
--- analysis/tokens.go
+++ analysis/tokens.go
@@ -2,6 +2,7 @@ package analysis
 
 import (
 	"borm-lsp/lsp"
+	"fmt"
 	"strings"
 )
 
@@ -43,6 +44,86 @@ func GetTokensToNewLine(tokens []Token) []Token {
 		idx++
 	}
 	return tokens[:idx] 
+}
+
+func GetFunctionTokens(tokens []Token, declaration string, l int) ([]Token, error) {
+	name, params, found := strings.Cut(declaration, "(")
+	if !found {
+		//is bad - declaration always needs name and brackets
+		return tokens, fmt.Errorf("Malformed function.")
+	}
+
+	nameParts := strings.Split(name, " ")
+	if len(nameParts) < 3 {
+		//is bad - should be: [1]type [2]function [3]name(params)
+		return tokens, fmt.Errorf("Malformed function.")
+	}
+
+	params, rest, found := strings.Cut(params, ")")
+	if !found {
+		// same bad as first bad
+		return tokens, fmt.Errorf("Malformed function.")
+	}
+
+	rest = strings.TrimSpace(rest)
+	if rest[0] == ';' {
+		// it's only a delcaration
+		return tokens[:l], nil
+	}
+	
+	if l >= len(tokens)-1 {
+		// bad no body and no semi colon
+		return tokens, fmt.Errorf("Function declarations must be closed with a semi colon.")
+	}
+		
+	return tokens, nil
+}
+
+func GetFunctionBodyTokens(tokens []Token) ([]Token, bool) {
+	depth := 0
+	inCommentLine := -1
+	inBody := false
+	inQuotes := false
+	inSingles := false
+	inAccent := false
+
+	for i := 0; i < len(tokens); i++ {
+		if inCommentLine == tokens[i].pos.Line {
+			continue
+		}
+		val := tokens[i].value
+		if cIdx := strings.Index(val, "//"); cIdx >= 0 {
+			// it's a comment disregard until new line
+			inCommentLine = tokens[i].pos.Line
+			//process whatever is before the slashes
+			val = val[:cIdx]
+		}
+		for _, char := range val {
+			if char == '"' && !inAccent && !inSingles {
+				inQuotes = !inQuotes
+			}
+			if char == '\'' && !inQuotes && !inAccent {
+				inSingles = !inSingles
+			}
+			if char == '`' && !inQuotes && !inSingles {
+				inAccent = !inAccent
+			}
+			if inQuotes || inSingles || inAccent {
+				continue
+			}
+			if char == '{' {
+				inBody = true
+				depth++
+			}
+			if char == '}' {
+				depth--
+			}
+			if inBody && depth == 0 {
+				return tokens[:i], true
+			}
+		}
+	}
+	return tokens, false
 }
 
 func Stringify(tokens []Token) string {
